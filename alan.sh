9c4b3d8c957f54da49293dfdd299bd2ab28e592c
Vlassis Ld <vlassis.ld@gmail.com>
2020-05-12 18:48:50 +0300

    comment alan script

--- alan.sh
+++ alan.sh
@@ -24,6 +24,7 @@ OPTSPEC="Olfih"
 while getopts "$OPTSPEC" FLAG; do
     case $FLAG in
         O)
+            # enable both optimizations
             alO="-O"
             llO="-O3"
             ;;
@@ -48,29 +49,44 @@ done
 shift $((OPTIND - 1))
 
 if [[ -z "$1" ]]; then
+    # input/output from stdin
+
     if [[ "$i" == "true" ]]; then
+        # output intermediate code
         "$alan" "$alO" <&0
+
     elif [[ "$f" == "true" ]]; then
-	tmpfile=$(mktemp -t alan.XXXXXX)
-	exec 3>"$tmpfile"
-	exec 4<"$tmpfile"
-	rm "$tmpfile"
+
+        # output intermediate code to temp
+        tmpfile=$(mktemp -t alan.XXXXXX)
+        exec 3>"$tmpfile"
+        exec 4<"$tmpfile"
+        rm "$tmpfile"
         "$alan" "$alO" <&0 >&3
-	$llc <&4
+        # input from temp, output target code to stdout
+        $llc $llO <&4
     else
         usage 1
     fi
 else
+    # input from file, no f/i flags should be set
     if [[  "$i" == "true" ]] || [[ "$f" == "true" ]]; then
         usage 1
     fi
+
+    # remove suffix
     base="${1%.*}"
+
+    # output intermediate code to .ll
     if ! "$alan" "$alO" < "$1" > "${base}.ll"; then
-    cat "${base}.ll";
-	rm "${base}.ll";
-	exit 1;
+        # remove .ll and exit if failed
+        rm "${base}.ll";
+        exit 1;
     fi
-    $llc < "${base}.ll" > "${base}.s"
-    clang "${base}.s" lib/lib.a -o "$base"
-    #clang -no-pie "${base}.s" lib/lib.a > "$base"
+
+    # input from .ll, output assembly to .s
+    $llc $llO < "${base}.ll" > "${base}.s"
+    # create final executable
+    clang -no-pie "${base}.s" lib/lib.a -o "$base"
+    echo "Done, executable in $base"
 fi
